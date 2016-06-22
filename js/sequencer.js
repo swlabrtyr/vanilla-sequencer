c27d82c89e30bc67b36b5c99fe380686d6dd0595
Thomas Roberson <thomas_roberson@icloud.com>
2016-06-22 17:15:08 +0800

    added envelope and oscillator detuning

--- js/sequencer.js
+++ js/sequencer.js
@@ -7,12 +7,13 @@ var current16thNote; // What note is currently last scheduled?
 var tempo = 120.0; // tempo (in beats per minute)
 
 var nextNoteTime = 0.0; // when the next note is due.
-
+var startTime = 0.25;
+var endTime = startTime + 1.5;
 var notesInQueue = []; // the notes that have been put into the web audio,
 // and may or may not have played yet. {note, time}
 var timerWorker = null; // The Web Worker used to fire timer messages
 var waveform, waveformChoice = 0;
-var osc, note, pitch = 440, noteChoice = 0;
+var pitch;
 var buttonArray = [], noteChoiceArray = [], pitchSelectArray = [];
 
 function init() {
@@ -39,7 +40,8 @@ function init() {
             {
                 ID: "select-" + i,
                 notes: [],
-                value: 440
+                // initial note value
+                value: 110
             }
         );
         // populate notes array
@@ -64,37 +66,53 @@ function init() {
 }
 
 function notePicker(value) {
-    return Math.pow(2, (value + 1 * 69 - 69) / 12) * 440;
+    return Math.pow(2, (value + 1 * 69 - 69) / 12) * 110;
 }
 
 var pitchSelector = document.getElementById("pitch-select-container");
 pitchSelector.addEventListener("change", selectPitch);
 
-var startValue = 0.8;
-var startTime = 0.2;
-var endValue = 0.0001;
-var endTime = startTime + 0.2;
-
 var lookahead = startTime + 0.1; // How frequently to call scheduling function
 // (in milliseconds)
 
-function createOsc(waveshape, note) {
-    var osc = audioContext.createOscillator();
+function createOsc1(waveshape, note, detune) {
+    var osc1 = audioContext.createOscillator();
+    osc1.type = waveshape;    
+    osc1.frequency.value = note;
+    osc1.detune.value = detune;
+    return osc1;
+}    
 
-    osc.type = waveshape;
-    osc.frequency.value = note;
-    
-    return osc;
+function createOsc2(waveshape, note, detune) {
+    var osc2 = audioContext.createOscillator();
+    osc2.type = waveshape;    
+    osc2.frequency.value = note;
+    osc2.detune.value = detune;
+    return osc2;
 }    
 
 function scheduleNote(beatNumber, time, wave, note) {
-    var osc = createOsc(wave, note);
-    console.log(osc);
-    osc.start(time);
-    osc.stop(time + endTime);
-
+    var osc1 = createOsc1(wave, note, 3);
+    var osc2 = createOsc2(wave, note, 10);
+    var attack = 0.1;
+    var decay = 0.8;
+    var sustain = 0.8;
+    
+    console.log("1st oscillator: " + osc1);
+    console.log("2nd oscillator: " + osc2);
+    osc1.start(time);
+    osc2.start(time);
+    
     var gain = audioContext.createGain();
-    osc.connect(gain);
+    osc1.connect(gain);
+    osc2.connect(gain);
+    // envelope
+    gain.gain.setValueAtTime(0.001, time);
+    gain.gain.exponentialRampToValueAtTime(0.5, time + attack);
+    gain.gain.exponentialRampToValueAtTime(0.001, time + sustain + decay);
+    // add small padding at the end (0.01) to prevent clipping when oscillator stops
+    osc1.stop(time + sustain + decay + 0.1);
+    osc2.stop(time + sustain + decay + 0.1);
     gain.connect(audioContext.destination);
     
     console.log(time + endTime);
